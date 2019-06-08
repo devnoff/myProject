7dabaf383c7480e75ecf41ab93f49122f0c4f6e3
Robinson Park <devnoff@gmail.com>
2019-06-08 16:50:37 +0900

    Auto Deploy Testing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const title = 'Hello World!! Auto Deployment Test Again! 06/07';
+const title = 'Hello World!! Auto Deployment Test Again! 06/08';
 
 ReactDOM.render(
   <div>{title}</div>, 
