ffcfc1339c61bcb67037f8697e5410e15f42cf03
Robinson Park <devnoff@gmail.com>
2019-06-11 18:04:26 +0900

    Update webpack configuration

--- webpack.common.js
+++ webpack.common.js
@@ -19,7 +19,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: '[name].bundle.js'
+    filename: '[name].[chunkhash].bundle.js'
   },
   plugins: [
     new CleanWebpackPlugin(),
